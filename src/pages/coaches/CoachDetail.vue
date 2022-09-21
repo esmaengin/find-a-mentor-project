2dc9bbb128430c198f2476f8c79cde5504771caa
Esma Engin <esmaengin91@gmail.com>
2022-09-21 15:00:10 +0300

    Created filter component

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,5 +1,55 @@
 <template>
-    MENTOR DETAY SAYFASI
-    <router-view></router-view>
-    <router-link to= "/coaches/c1/contact">Bağlantı Kur!</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3> ${{ rate }}/saat</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+          <h2>Mentorümüzle tanışmak ister misin?</h2>
+          <base-button link :to="contactLink">İletişim</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge v-for="area in areas" :key="area" :title="area">
+      </base-badge>
+        <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script>
+export default {
+    props: ['id'],
+    data(){
+        return{
+            selectedCoach: null
+        };
+    },
+    computed: {
+        fullName() {
+            return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+        },
+        areas(){
+            return this.selectedCoach.areas;
+        },
+        rate(){
+            return this.selectedCoach.hourlyRate;
+        },
+        description(){
+            return this.selectedCoach.description;
+        },
+        contactLink(){
+            return this.$route.path + '/' + this.id + '/contact';
+        },
+    },
+    created(){
+        this.selectedCoach = this.$store.getters['coaches/coaches'].find(coach => coach.id === this.id);
+    },
+};
+</script>
