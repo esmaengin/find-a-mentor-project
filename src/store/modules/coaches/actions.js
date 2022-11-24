214a1ff182f00f1b043cb0232d63c6d051c5f9f7
Esma Engin <esmaengin91@gmail.com>
2022-11-24 23:49:11 +0300

    mentor requests

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -26,7 +26,7 @@ export default {
         });
     },
     async loadCoaches(context) {
-        const response = await fetch(`https://find-a-mentor-882e3-default-rtdb.firebaseio.com/coaches.jso`);
+        const response = await fetch(`https://find-a-mentor-882e3-default-rtdb.firebaseio.com/coaches.json`);
         const responseData = await response.json();
 
         if(!response.ok) {
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,48 @@
 export default {
-    contactCoach (context, payload) {
+    async contactCoach (context, payload) {
         const newRequest = {
-            id: new Date().toISOString(),
-            coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message
         };
+        const response = await fetch(`https://find-a-mentor-882e3-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`, {
+            method: 'POST',
+            body: JSON.stringify(newRequest)
+        });
+
+        const responseData = await response.json();
+
+        if(!response.ok) {
+            const error = new Error(responseData.message || 'İstek gönderilemedi!')
+            throw error;
+        }
+
+        newRequest.id = responseData.name;
+        newRequest.coachId = payload.coachId;
+
         context.commit('addRequest', newRequest);
+    },
+    async fetchRequests(context) {
+        const coachId  = context.rootGetters.userId;
+        const response = await fetch(`https://find-a-mentor-882e3-default-rtdb.firebaseio.com/requests/${coachId}.json`);
+        const responseData = await response.json();
+
+        if(!response.ok) {
+            const error = new Error(responseData.message || 'Başarısız istek!');
+            throw error;
+        }
+
+        const requests = [];
+
+        for (const key in responseData) {
+            const request = {
+                id: key,
+                coachId: coachId, 
+                userEmail: responseData[key].userEmail,
+                message: responseData[key].message,
+            };
+            requests.push(request);
+        }
+
+        context.commit('setRequests', requests);
     }
 };
