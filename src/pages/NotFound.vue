06c8103172c746b8cc2bfbb72b5e666516c1e998
Esma Engin <esmaengin91@gmail.com>
2022-11-29 00:19:04 +0300

    developed not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Sayfa bulunamadı.</h2>
+            <p>Bu sayfa bulunamadı - <router-link to="/coaches">Mentorlerimizi </router-link> kontrol edebilirsin!</p>
+        </base-card>
+    </section>
+</template>
